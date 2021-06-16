f38349b44b27a19f12e29bb79d9296a72181d1a6
NaufalAkbar10 <72178345+NaufalAkbar10@users.noreply.github.com>
2021-06-16 21:16:22 +0700

    Update landing.blade.php

--- laravel_icomit_fiveo/resources/views/frontend/landing.blade.php
+++ laravel_icomit_fiveo/resources/views/frontend/landing.blade.php
@@ -213,6 +213,17 @@
                     </div>
                 </div>
             </div>
+             <div class="col-lg-4">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="{{ asset('frontend/assets/img/team/3.jpg') }}"
+                            alt="..." />
+                        <h4>Syahbana Ramadhan</h4>
+                        <p class="text-muted">Lead Developer</p>
+                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
             <div class="row">
                 <div class="col-lg-8 mx-auto text-center">
                     <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque,
